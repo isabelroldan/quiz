825b9246a92ff1ccf5eef2a4880678bd34c58bb2
Isabel <isabelroldancordoba@hotmail.com>
2024-01-11 16:49:59 +0100

    Eliminación de importaciones no usadas

--- src/main/java/org/example/interfaces/Menu.java
+++ src/main/java/org/example/interfaces/Menu.java
@@ -5,11 +5,9 @@ import org.example.model.QuizGame;
 import org.example.model.Utilities;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
